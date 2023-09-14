d544f36bfc560d70d1ce7d5994e7dff8cb3d7aa1
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2023-09-14 22:05:34 +0200

    u(cmake): allow to specify VCPKG_BUILTIN_BASELINE/BOOTSTRAP_CHECKOUT
    VCPKG_BUILTIN_BASELINE - manifest builtin baseline
    VCPKG_BOOTSTRAP_CHECKOUT - vcpkg repo checkout, by default uses
    VCPKG_BUILTIN_BASELINE

--- cmake/vcpkg/vcpkg.toolchain.cmake
+++ cmake/vcpkg/vcpkg.toolchain.cmake
@@ -5,11 +5,32 @@ include_guard(GLOBAL)
 
 cmake_minimum_required(VERSION 3.25)
 
+get_property(IN_TRY_COMPILE GLOBAL PROPERTY IN_TRY_COMPILE)
+
+if (IN_TRY_COMPILE)
+  return()
+endif()
+
+unset(IN_TRY_COMPILE)
+
+if (NOT DEFINED VCPKG_BUILTIN_BASELINE)
+  message(FATAL_ERROR "Missing mandatory VCPKG_BUILTIN_BASELINE")
+endif()
+
+if (NOT DEFINED VCPKG_BOOTSTRAP_CHECKOUT)
+  set(VCPKG_BOOTSTRAP_CHECKOUT "${VCPKG_BUILTIN_BASELINE}" CACHE INTERNAL "")
+endif()
+
+if (EXISTS "${CMAKE_SOURCE_DIR}/vcpkg.json")
+  configure_file(${CMAKE_SOURCE_DIR}/vcpkg.json "." @ONLY)
+  set(VCPKG_MANIFEST_DIR ${CMAKE_BINARY_DIR} CACHE INTERNAL "")
+endif()
+
 include(${CMAKE_CURRENT_LIST_DIR}/vcpkg_bootstrap.cmake)
 
 vcpkg_bootstrap(
   https://github.com/microsoft/vcpkg.git
-  9edb1b8e590cc086563301d735cae4b6e732d2d2 # 2023.08.09
+  "${VCPKG_BOOTSTRAP_CHECKOUT}"
 )
 
 include($CACHE{VCPKG_TOOLCHAIN_FILE})
