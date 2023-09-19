77d027c03504de98f6ae4a04b24379976ef04927
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2023-09-20 00:00:51 +0200

    fix(cmake): genreate export headers only for shared/static libs

--- src/cmake/add_cpp_pt_module.cmake
+++ src/cmake/add_cpp_pt_module.cmake
@@ -8,6 +8,23 @@ include(set_@cpp_pt_cmake@_target_properties)
 include(GenerateExportHeader)
 include(GNUInstallDirs)
 
+# generaete header with export macro
+function(_@cpp_pt_cmake@_module_generate_export_headers target)
+  set(export_file_dir "${CMAKE_CURRENT_BINARY_DIR}/include/@cpp_pt_name@")
+  generate_export_header(${module_target}
+    EXPORT_FILE_NAME "${export_file_dir}/${module_name}/export.hpp"
+  )
+
+  target_include_directories(
+    ${module_target} ${module_type}
+    $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/include>
+  )
+
+  if (TARGET @cpp_pt_name@)
+    install(DIRECTORY ${export_file_dir} TYPE INCLUDE)
+  endif()
+endfunction()
+
 # sets all nessary default things
 function(add_@cpp_pt_cmake@_module module_name)
   set(module_target @cpp_pt_cmake@-${module_name})
@@ -21,14 +38,10 @@ function(add_@cpp_pt_cmake@_module module_name)
     set(module_type "INTERFACE")
   else()
     set(module_type "PUBLIC")
+    _@cpp_pt_cmake@_module_generate_export_headers(${module_target})
   endif()
 
   set_@cpp_pt_cmake@_target_properties(${module_target} ${module_type})
-
-  set(export_file_dir "${CMAKE_CURRENT_BINARY_DIR}/include/@cpp_pt_name@")
-  generate_export_header(${module_target}
-    EXPORT_FILE_NAME "${export_file_dir}/${module_name}/export.hpp"
-  )
 
   target_include_directories(
     ${module_target} ${module_type}
@@ -44,7 +57,6 @@ function(add_@cpp_pt_cmake@_module module_name)
     target_link_libraries(@cpp_pt_name@ INTERFACE ${module_target})
     install(TARGETS ${module_target} EXPORT @cpp_pt_name@-targets)
     install(DIRECTORY include/@cpp_pt_name@ TYPE INCLUDE)
-    install(DIRECTORY ${export_file_dir} TYPE INCLUDE)
   endif()
 
   set(@cpp_pt_cmake@_module_target
