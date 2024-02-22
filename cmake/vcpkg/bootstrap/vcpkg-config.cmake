5ba7a75c25031f3fac83b14a6dd7f888fcdde1aa
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2024-02-22 22:05:53 +0100

    feat(cmake/vcpkg): introduce _vcpkg_skip_install_on_reconfigure
    Disable vcpkg manifest install step on cmake reconfigure if vcpkg.json has not
    changed.
    
    This is quite usefull with:
    - `cmake --workflows --preset <name>` which currently runs configure step always
    - when cmake --build triggers re-configure

--- cmake/vcpkg/bootstrap/vcpkg-config.cmake
+++ cmake/vcpkg/bootstrap/vcpkg-config.cmake
@@ -11,5 +11,6 @@ set(__vcpkg_bootstrap_list_dir "${CMAKE_CURRENT_LIST_DIR}")
 
 include(${__vcpkg_bootstrap_list_dir}/vcpkg_autodetect_target_triplet.cmake)
 include(${__vcpkg_bootstrap_list_dir}/vcpkg_autodetect_host_triplet.cmake)
+include(${__vcpkg_bootstrap_list_dir}/vcpkg_skip_install_on_reconfigure.cmake)
 include(${__vcpkg_bootstrap_list_dir}/vcpkg_bootstrap.cmake)
 include(${__vcpkg_bootstrap_list_dir}/vcpkg_configure.cmake)
