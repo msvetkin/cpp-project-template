7daf40e341f682f762066fc2882a11cd4a2fb6ac
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2024-02-22 21:31:31 +0100

    refactor(cmake/vcpkg): rename PRESET_NAME_TO_VCPKG_TARGET_TRIPLET to VCPKG_PRESET_NAME
    The plan is to make VCPKG_PRESET_NAME as mandatory variable.

--- cmake/vcpkg/bootstrap/vcpkg_autodetect_target_triplet.cmake
+++ cmake/vcpkg/bootstrap/vcpkg_autodetect_target_triplet.cmake
@@ -3,13 +3,21 @@
 
 include_guard(GLOBAL)
 
-# sets VCPKG_TARGET_TRIPLET out of PRESET_NAME_TO_VCPKG_TARGET_TRIPLET
-function(vcpkg_autodetect_target_target_triplet)
-  if(APPLE)
-    string(REPLACE "darwin" "osx" PRESET_NAME_TO_VCPKG_TARGET_TRIPLET "${PRESET_NAME_TO_VCPKG_TARGET_TRIPLET}")
+# sets VCPKG_TARGET_TRIPLET out of VCPKG_PRESET_NAME
+function(vcpkg_autodetect_target_triplet)
+  if (DEFINED CACHE{VCPKG_TARGET_TRIPLET})
+    return()
   endif()
 
-  string(REPLACE "-" ";" triplet_parts "${PRESET_NAME_TO_VCPKG_TARGET_TRIPLET}")
+  if (NOT DEFINED VCPKG_PRESET_NAME)
+    message(FATAL_ERROR "Missing mandatory VCPKG_PRESET_NAME cache variable")
+  endif()
+
+  if(APPLE)
+    string(REPLACE "darwin" "osx" VCPKG_PRESET_NAME "${VCPKG_PRESET_NAME}")
+  endif()
+
+  string(REPLACE "-" ";" triplet_parts "${VCPKG_PRESET_NAME}")
 
   # remove compiler
   list(REMOVE_AT triplet_parts 2)
@@ -25,5 +33,6 @@ function(vcpkg_autodetect_target_target_triplet)
   list(JOIN triplet_parts "-" vcpkg_target_triplet)
 
   message(STATUS "Auto detected VCPKG_TARGET_TRIPLET: ${vcpkg_target_triplet}")
-  set(VCPKG_TARGET_TRIPLET "${vcpkg_target_triplet}" CACHE STRING "Auto detected vcpkg target triplet")
+  set(VCPKG_TARGET_TRIPLET "${vcpkg_target_triplet}"
+      CACHE STRING "Auto detected vcpkg target triplet")
 endfunction()
