3599a9a3b7c0914752578997dfa263efe1bc3d5e
Rio Martinez <rio@riomartinez.com>
2024-03-22 14:58:56 +0100

    chore(ci): remove libc++ and libc++abi on x86

--- cmake/vcpkg/triplets/x86-linux-clang-dynamic.cmake
+++ cmake/vcpkg/triplets/x86-linux-clang-dynamic.cmake
@@ -3,7 +3,3 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
-
-set(VCPKG_CXX_FLAGS -stdlib=libc++)
-set(VCPKG_C_FLAGS -stdlib=libc++)
-set(VCPKG_LINKER_FLAGS "-stdlib=libc++")
